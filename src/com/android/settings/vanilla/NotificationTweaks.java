0f23ef3991ae1199b5aaf545386733c83d599958
ezio84 <brabus84@gmail.com>
2017-03-11 20:11:50 +0100

    Heads up and Notification Ticker: handle both with a ListPref

--- src/com/android/settings/vanilla/NotificationTweaks.java
+++ src/com/android/settings/vanilla/NotificationTweaks.java
@@ -16,26 +16,83 @@
 
 package com.android.settings.vanilla;
 
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class NotificationTweaks extends SettingsPreferenceFragment {
+public class NotificationTweaks extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String NOTIFICATION_MODE = "notification_mode";
+
+    private ListPreference mNotificationMode;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.notification_tweaks);
         PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mNotificationMode = (ListPreference) findPreference(NOTIFICATION_MODE);
+        mNotificationMode.setOnPreferenceChangeListener(this);
+        int headsupMode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                1, UserHandle.USER_CURRENT);
+        int tickerMode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_TICKER,
+                0, UserHandle.USER_CURRENT);
+        int notificationMode;
+        if (headsupMode == 1) {
+            notificationMode = 0;
+        } else if (tickerMode == 1) {
+            notificationMode = 1;
+        } else {
+            notificationMode = 2;
+        }
+        mNotificationMode.setValue(String.valueOf(notificationMode));
+        mNotificationMode.setSummary(mNotificationMode.getEntry());
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.VANILLA;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mNotificationMode)) {
+            int notificationMode = Integer.parseInt(((String) newValue).toString());
+            int headsupMode;
+            int tickerMode;
+            if (notificationMode == 0) {
+                headsupMode = 1;
+                tickerMode = 0;
+            } else if (notificationMode == 1) {
+                headsupMode = 0;
+                tickerMode = 1;
+            } else {
+                headsupMode = 0;
+                tickerMode = 0;
+            }
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.HEADS_UP_USER_ENABLED, headsupMode, UserHandle.USER_CURRENT);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_TICKER, tickerMode, UserHandle.USER_CURRENT);
+            int index = mNotificationMode.findIndexOfValue((String) newValue);
+            mNotificationMode.setSummary(
+                    mNotificationMode.getEntries()[index]);
+            return true;
+        }
+        return false;
     }
 }
 
